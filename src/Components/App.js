759874e4e88be77577fceb88dc472a10d5178843
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-06-25 18:54:31 +0300

    Conturol Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import Main from '../Assets/Style/Main.css';
 import Progress from './TheProgress.js';
 import Options from './TheOptions.js';
+import Control from './TheControl.js';
 
 import { useState } from 'react'
 function App() {
@@ -12,7 +13,8 @@ function App() {
             <div className="questContent">
                 <h1>An interface design application that runs in the browser with team-based collaborative design projects</h1>
             </div>
-            <Options options={['Test1', 'Test2', 'Test3', 'Test4', 'Test5', 'Test6']}/>
+            <Options options={['Test1', 'Test2', 'Tesft3', 'Test4', 'Test5', 'Test6']}/>
+            <Control/>
         </div>
     </div>
   );
---
+++ src/Components/TheControl.js
@@ -0,0 +1,10 @@
+
+function Control() {
+	return (
+		<div className="controlContent">
+			<h1>Test</h1>
+		</div>
+	)
+}
+
+export default Control
