d80049012798e7f9f142eb55ef266a11d428c8e4
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-06-25 19:40:57 +0300

    Svg Icons

--- src/Components/TheControl.js
+++ src/Components/TheControl.js
@@ -1,14 +1,24 @@
 import Time from '../Assets/Icons/Time.svg';
-import Arrow from '../Assets/Icons/Arrow.svg';
+import { ReactComponent as Arrow}  from '../Assets/Icons/Arrow.svg';
+import Skip from '../Assets/Icons/Skip.svg';
 
 function Control() {
 	return (
 		<div className="controlContent">
-			<span className="previous button">Previous</span>
+			<span className="previous button">
+			    <Arrow className="mext-previous"/>
+			    Previous
+			</span>
 			<span><img src={Time}/></span>
 			<div>
-				<span className="next button">Next</span>
-				<span className="skip-button">Skip</span>
+				<span className="next button">
+				    Next
+				    <Arrow className="mext-arrow"/>				    
+				</span>
+				<span className="skip button">
+				    Skip
+				    <img src={Skip}/>
+				</span>
 			</div>
 		</div>
 	)
