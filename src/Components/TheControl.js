75a515a7e69d98d8ba32e4fa22cecba31d76c8a1
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-06-29 08:07:13 +0300

    Disabled Button

--- src/Components/TheControl.js
+++ src/Components/TheControl.js
@@ -1,41 +1,58 @@
 import Time from '../Assets/Icons/Time.svg';
 import { ReactComponent as Arrow}  from '../Assets/Icons/Arrow.svg';
 import { ReactComponent as Skip} from '../Assets/Icons/Skip.svg';
-import { useState, useEffect } from 'react'
+import { useState, useLayoutEffect } from 'react'
 
 function Control({selectedQuestion, setSelectedQuestion}) {
 
-	const [hiddenSkipButton, setSkipButton] = useState('');
-	const [hiddenPreviousButton, setPreviousButton] = useState('')
+	const [hiddenButton, setHiddenButton] = useState({
+		previousButton: '',
+		previousButtonDisabled: false,
+		skipButton: '',
+		skipButtonDisabled: false,
+	});
     
-    useEffect(() => {
+    useLayoutEffect(() => {
     	if(selectedQuestion >= 5) {
-    		setSkipButton('hiddenContent')
+    		setHiddenButton({
+    			...hiddenButton,
+    			skipButton: 'hiddenContent',
+    			skipButtonDisabled: true})
     	}else if(selectedQuestion == 0){
-    		setPreviousButton('hiddenContent')
+    		setHiddenButton({
+    			...hiddenButton,
+    			previousButton: 'hiddenContent',
+    			previousButtonDisabled: true})
     	}else {
-    		setPreviousButton('');
-    		setSkipButton('');
+    		setHiddenButton({
+    			...hiddenButton,
+		        previousButton: '',
+		        previousButtonDisabled: false,
+		        skipButton: '',
+		        skipButtonDisabled: false,
+	        });
     	}
     }, [selectedQuestion])
 
 
 	return (
 		<div className="controlContent">
-			<span className={'previous button ' + hiddenPreviousButton} onClick={() => setSelectedQuestion(selectedQuestion -= 1)}>
+			<button className={'previous button ' + hiddenButton.previousButton} onClick={() => setSelectedQuestion(selectedQuestion -= 1)} disabled={hiddenButton.previousButtonDisabled}>
 			    <Arrow className="arrowIcon"/>
 			    Previous
+			</button>
+			<span>
+			    <img src={Time} draggable="false"/>
 			</span>
-			<span><img src={Time}/></span>
 			<div>
-				<span className="next button" onClick={() => setSelectedQuestion(selectedQuestion += 1)}>
+				<button className="next button" onClick={() => setSelectedQuestion(selectedQuestion += 1)}>
 				    Next
 				    <Arrow className="arrowIcon"/>				    
-				</span>
-				<span className={'skip button ' + hiddenSkipButton} onClick={() => setSelectedQuestion(selectedQuestion += 1)}>
+				</button>
+				<button className={'skip button ' + hiddenButton.skipButton} onClick={() => setSelectedQuestion(selectedQuestion += 1)} disabled={hiddenButton.skipButtonDisabled}>
 				    Skip
 				    <Skip/>
-				</span>
+				</button>
 			</div>
 		</div>
 	)
