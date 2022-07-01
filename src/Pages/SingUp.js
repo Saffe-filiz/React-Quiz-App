0b0f5975179b763b54133b39d1d4add15b6235b7
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-07-01 07:07:49 +0300

    From

--- src/Pages/SingUp.js
+++ src/Pages/SingUp.js
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { singUp } from '../authentication.js';
 
 function SingUp () {
-	const [email, setEmail] = useState()
-	const [passworld, setPassworld] = useState()
+	const [email, setEmail] = useState('')
+	const [passworld, setPassworld] = useState('')
 
     const newUser = (e) => {
     	e.preventDefault();
     	singUp(email, passworld);
+    	setEmail('')
+    	setPassworld('')
     }
 
 	return (
@@ -15,12 +17,13 @@ function SingUp () {
 			<form onSubmit={newUser}>
 				<span>
 					<label htmlFor="email"></label>
-					<input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)}/>
-				</span>
+					<input type="text" id="email" value={email} onChange={e => setEmail(e.target.value)}/>
+				</span><br/>
 				<span>
 					<label htmlFor="password"></label>
 					<input type="password" id="password" value={passworld} onChange={e => setPassworld(e.target.value)}/>
 				</span>
+				<button type="submit">Send</button>
 			</form>
 		</div>
 	)
