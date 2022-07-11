c278636e12172ed3c4552f31198929b16342c0f0
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-07-11 18:58:40 +0300

    Quiz List

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -5,25 +5,52 @@ import Navigation from '../Components/TheNavigation.js';
 import ContentBgImage from '../Assets/Icons/ContentBgImage.png'
 
 import { useSelector } from 'react-redux'
-import { useNavigate } from "react-router-dom";
 
-import { useState, useLayoutEffect } from 'react'
+import { useState } from 'react'
+
+   
 
 function Quiz() {
-    const  { readyToQuiz } = useSelector((state) => state.user)
-    const navigate = useNavigate();
-
-
     const [selectedQuestion, setSelectedQuestion] = useState(0);
+     const [questions, setQuestions] = useState([
+      {
+        question: '666 - 333',
+        suggestions: [
+            {suggestion: '223'},
+            {suggestion: '333', currect: true},
+            {suggestion: '444'},
+            {suggestion: '555'},
+        ]
+    },
+     {
+        question: '25234 - 2334',
+        suggestions: [
+            {suggestion: '22.400'},
+            {suggestion: '21.521'},
+            {suggestion: '16.362'},
+            {suggestion: '22.900', currect: true},
+        ]
+      },
+       {
+        question: '5 * 10',
+        suggestions: [
+            {suggestion: '50', currect: true},
+            {suggestion: '55'},
+            {suggestion: '60'},
+            {suggestion: '45'},
+        ]
+      }
+    ])
 
     return (
         <div className="container" style={{backgroundImage: `url(${ContentBgImage})`}}>
             <Navigation/>
             {/*<Progress questLength={5}/>*/}
-            <div className="questContent">
-                <h1>An interface design application that runs in the browser with team-based collaborative design projects{selectedQuestion}</h1>
-            </div>
-            <Options options={['Test1', 'Test2', 'Tesft3', 'Test4', 'Test5', 'Test6']}/>
+            {Object.keys(questions).map((key, index ) => {
+                    <div className="questContent">
+                        <h1>questions[key].question</h1>
+                    </div>
+            }).slice(0,1)}
             <Control 
                 selectedQuestion={selectedQuestion} 
                 setSelectedQuestion={setSelectedQuestion}/>
