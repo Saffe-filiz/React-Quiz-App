7e5a2d1d541be70a81c4e3680d39938932d92b85
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-07-12 18:15:37 +0300

    Active Button

--- src/Components/TheOptions.js
+++ src/Components/TheOptions.js
@@ -3,19 +3,19 @@ import { useState } from 'react';
 let choice = ['A', 'B,', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 function Options({options, index}) {
-	const [answers, setAnswers] = useState([]);
+	const [answers, setAnswers] = useState();
 
 	const selecAnswer = ( value ) => {
 		
-		setAnswers(pre => [...pre, value]);
-		console.log(answers,index )
+		setAnswers(value);
+		console.log(answers,value )
 	}
 
 	return (
 		<div className="optionsContent">
 			<div className="quizOptions">
 				{options.map(({suggestion}, index) => (
-					<div className={answers[index] == suggestion ? 'options isActive': 'options'}  key={index} onClick={() => selecAnswer(suggestion)}>
+					<div className={answers == suggestion ? 'options isActive': 'options'}  key={index} onClick={() => selecAnswer(suggestion)}>
 					    <span className="choiceName">{choice[index]}.</span> 
 					     <p>{suggestion}</p>
 					</div>
